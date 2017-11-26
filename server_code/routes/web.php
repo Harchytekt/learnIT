aac611a1afe97a9c59c75d1c1191c70d8d9e2efb
Alexandre Ducobu <Harchytekt@users.noreply.github.com>
2017-11-26 17:46:19 +0100

    Add auth middleware for all pages

--- server_code/routes/web.php
+++ server_code/routes/web.php
@@ -24,40 +24,40 @@ Route::get('/accueil', 'HomeController@index')->name('accueil');
 
 Route::get('/cours', function() {
     return view('cours');
-});
+})->middleware('auth');
 
 Route::get('/ecrire', function() {
     return view('ecrire');
-});
+})->middleware('auth');
 
 Route::get('/compte', function() {
     return view('compte');
-});
+})->middleware('auth');
 
 /* Mes cours */
 
 Route::get('/coursinscrits', function() {
     return view('mesCours.inscrits');
-});
+})->middleware('auth');
 
 Route::get('/favoris', function() {
     return view('mesCours.favoris');
-});
+})->middleware('auth');
 
 Route::get('/encours', function() {
     return view('mesCours.encours');
-});
+})->middleware('auth');
 
 Route::get('/coursecrits', function() {
     return view('mesCours.ecrits');
-});
+})->middleware('auth');
 
 /* Mes chiffres */
 
 Route::get('/chiffresinscrits', function() {
     return view('mesChiffres.inscrits');
-});
+})->middleware('auth');
 
 Route::get('/chiffresecrits', function() {
     return view('mesChiffres.ecrits');
-});
+})->middleware('auth');
