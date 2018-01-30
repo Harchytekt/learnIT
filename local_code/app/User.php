6dfbee9c150d60456c876a74b76dba62a23b70b7
Alexandre Ducobu <Harchytekt@users.noreply.github.com>
2018-01-30 14:08:57 +0100

    :sparkles: Update User model for dates

--- local_code/app/User.php
+++ local_code/app/User.php
@@ -10,12 +10,23 @@ class User extends Authenticatable
     use Notifiable;
 
     /**
+     * Used to convert the dates columns to instances of Carbon.
+     * Thanks to this, we can use diffForHumans(), toFormattedDateString(),...
+     *
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'lastLogin_at'
+    ];
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'username', 'email', 'password', 'lastname', 'firstname', 'tutor', 'active',
+        'username', 'email', 'password', 'lastname', 'firstname',
     ];
 
     /**
@@ -26,4 +37,12 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function isATutor() {
+        return $this->tutor == 1;
+    }
+
+    public function isActive() {
+        return $this->active == 1;
+    }
 }
