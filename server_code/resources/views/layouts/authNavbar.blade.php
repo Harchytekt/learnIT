e4db5ef17bc4efc4a74d0dc81d1a664397af35a9
Alexandre Ducobu <Harchytekt@users.noreply.github.com>
2018-02-02 12:36:58 +0100

    :sparkles: :lipstick: Add spin on icon hover

--- server_code/resources/views/layouts/authNavbar.blade.php
+++ server_code/resources/views/layouts/authNavbar.blade.php
@@ -10,35 +10,35 @@
     <div class="collapse navbar-collapse" id="navbarToggler">
         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
             <li class="nav-item">
-                <a class="nav-link" href="/cours"><i class="fas fa-university"></i> Cours</a>
+                <a class="nav-link fa-spin-hover" href="/cours"><i class="fas fa-university"></i> Cours</a>
             </li>
             <li class="nav-item dropdown active">
-                <a class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"     aria-expanded="true" href="/mescours"><i class="fas fa-list-ul"></i>
+                <a class="nav-link dropdown-toggle fa-spin-hover" data-toggle="dropdown" role="button" aria-haspopup="true"     aria-expanded="true" href="/mescours"><i class="fas fa-list-ul"></i>
                 Mes cours</a>
                     <div class="dropdown-menu" x-placement="bottom-start" style="position: absolute; transform: translate3d(0px, 44px, 0px); top: 0px; left: 0px; will-change: transform;">
-                    <a class="dropdown-item" href="/coursinscrits"><i class="fas fa-list-ul"></i>
+                    <a class="dropdown-item fa-spin-hover" href="/coursinscrits"><i class="fas fa-list-ul"></i>
                     Inscrits</a>
                     <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="/favoris"><i class="fas fa-star"></i> Favoris</a>
-                    <a class="dropdown-item" href="/encours"><i class="far fa-clock"></i> En cours</a>
+                    <a class="dropdown-item fa-spin-hover" href="/favoris"><i class="fas fa-star"></i> Favoris</a>
+                    <a class="dropdown-item fa-spin-hover" href="/encours"><i class="far fa-clock"></i> En cours</a>
                     <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="/coursecrits"><i class="fas fa-edit"></i> Écrits</a>
+                    <a class="dropdown-item fa-spin-hover" href="/coursecrits"><i class="fas fa-edit"></i> Écrits</a>
                 </div>
             </li>
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"     aria-expanded="true" href="/chiffres"><i class="fas fa-chart-line"></i> Mes chiffres</a>
+                <a class="nav-link dropdown-toggle fa-spin-hover" data-toggle="dropdown" role="button" aria-haspopup="true"     aria-expanded="true" href="/chiffres"><i class="fas fa-chart-line"></i> Mes chiffres</a>
                     <div class="dropdown-menu" x-placement="bottom-start" style="position: absolute; transform: translate3d(0px, 44px, 0px); top: 0px; left: 0px; will-change: transform;">
-                    <a class="dropdown-item" href="/chiffresinscrits"><i class="far fa-chart-bar"></i>
+                    <a class="dropdown-item fa-spin-hover" href="/chiffresinscrits"><i class="far fa-chart-bar"></i>
                     Inscrits</a>
                     <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="/chiffresecrits"><i class="fas fa-chart-area"></i> Écrits</a>
+                    <a class="dropdown-item fa-spin-hover" href="/chiffresecrits"><i class="fas fa-chart-area"></i> Écrits</a>
                 </div>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="/ecrire"><i class="fas fa-pencil-alt"></i> Écrire un cours</a>
+                <a class="nav-link fa-spin-hover" href="/ecrire"><i class="fas fa-pencil-alt"></i> Écrire un cours</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="/compte"><i class="fas fa-sliders-h"></i> {{ Auth::user()->username }}</a>
+                <a class="nav-link fa-spin-hover" href="/compte"><i class="fas fa-sliders-h"></i> {{ Auth::user()->username }}</a>
             </li>
         </ul>
         <form class="form-inline my-2 my-lg-0">
@@ -49,7 +49,7 @@
                 </div>
             </div>
         </form>
-        <a class="btn btn-outline-danger" href="{{ route('logout') }}"
+        <a class="btn btn-outline-danger fa-spin-hover" href="{{ route('logout') }}"
             onclick="event.preventDefault();
                      document.getElementById('logout-form').submit();"><i class="fas fa-power-off"></i> Déconnexion
         </a>
