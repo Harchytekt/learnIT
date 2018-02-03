4ce62da4faba0dcb5944eef68460d6d35b8383f7
Alexandre Ducobu <Harchytekt@users.noreply.github.com>
2018-02-03 14:54:34 +0100

    :construction: Update Account page
    :sparkles: Add register date

--- server_code/resources/views/authenticated/compte.blade.php
+++ server_code/resources/views/authenticated/compte.blade.php
@@ -5,4 +5,7 @@
 @section('content')
     <h1 style="display: inline;">Votre compte </h1>
     <h6 style="display: inline;">(Dernière connexion {{ Auth::user()->lastLogin_at->diffForHumans() }})</h6>
+
+
+    <h4>Date d'inscription : {{ Auth::user()->created_at->formatLocalized('%d %B %Y') }}</h4>
 @endsection
