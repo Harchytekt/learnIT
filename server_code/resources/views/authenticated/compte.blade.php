2babf074f62557acc0ce4c1268b6d1363eff9ed3
Alexandre Ducobu <Harchytekt@users.noreply.github.com>
2018-02-04 19:12:26 +0100

    :iphone: Update responsiveness of the page

--- server_code/resources/views/authenticated/compte.blade.php
+++ server_code/resources/views/authenticated/compte.blade.php
@@ -28,7 +28,7 @@
                 <h6>(Dernière connexion {{ Auth::user()->lastLogin_at->diffForHumans() }})</h6>
             </div>
 
-            <div class="float-right">
+            <div class="float-left float-md-right">
                 <h4 id="createdAt">Inscription le {{ Auth::user()->created_at->formatLocalized('%A %d %B %Y') }}</h4>
             </div>
         </div>
@@ -37,31 +37,38 @@
             <img src="{{ asset('img/users/'.$img.'.svg') }}" alt="" height="128" width="128">
 
             <span class="vars name">Monsieur</span> <span class="vars name">X, {{ $status }}</span>
-            <button type="button" class="btn btn-link" id="modifyName">Modifier</button>
+            <!--<button type="button" class="btn btn-link" id="modifyName">Modifier</button>-->
+            <div id="centerBtnName">
+                <button type="button" class="btn btn-link" id="modifyName">Modifier</button>
+            </div>
         </div>
 
         <hr>
 
         <div class="clearfix">
-            <div class="float-left">
-                <h4>Changer votre adresse mail</h4>
-                <span class="label">Adresse mail actuelle :</span> <span class="vars">{{ Auth::user()->email }}</span> <br>
-                <label class="label" for="newEmail">Nouvelle adresse mail : </label>
-                <input type="email" name="newEmail" value=""> <br>
-                <div class="centerBtn"><button type="button" class="btn btn-link">Modifier</button></div>
+            <div class="centerModify">
+                <div class="float-left">
+                    <h4>Changer votre adresse mail</h4>
+                    <span class="label">Adresse mail actuelle :</span> <span class="vars">{{ Auth::user()->email }}</span> <br>
+                    <label class="label" for="newEmail">Nouvelle adresse mail : </label>
+                    <input type="email" name="newEmail" value=""> <br>
+                    <div class="centerBtn"><button type="button" class="btn btn-link">Modifier</button></div>
+                </div>
             </div>
 
-            <div class="float-right">
-                <div class="clearfix">
-                    <h4>Changer votre mot de passe</h4>
-                    <label class="label mdp" for="oldPwd">Ancien mot de passe : </label>
-                    <input type="password" name="oldPwd" value=""> <br>
-                    <label class="label mdp" for="newPwd1">Nouveau mot de passe : </label>
-                    <input type="password" name="newPwd1" value=""> <br>
-                    <label class="label mdp" for="newPwd2">Réécrire le nouveau mot de passe : </label>
-                    <input class="label" type="password" name="newPwd2" value="">
+            <div class="centerModify">
+                <div class="float-left float-lg-right">
+                    <div class="clearfix">
+                        <h4>Changer votre mot de passe</h4>
+                        <label class="label mdp" for="oldPwd">Ancien mot de passe : </label>
+                        <input type="password" name="oldPwd" value=""> <br>
+                        <label class="label mdp" for="newPwd1">Nouveau mot de passe : </label>
+                        <input type="password" name="newPwd1" value=""> <br>
+                        <label class="label mdp" for="newPwd2">Réécrire le nouveau mot de passe : </label>
+                        <input type="password" name="newPwd2" value="">
+                    </div>
+                    <div class="centerBtn"><button type="button" class="btn btn-link">Modifier</button></div>
                 </div>
-                <div class="centerBtn"><button type="button" class="btn btn-link">Modifier</button></div>
             </div>
         </div>
 
