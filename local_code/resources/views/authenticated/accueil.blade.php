2fe9d6f327730c6421a338255c23c4cedf620d9c
Alexandre Ducobu <Harchytekt@users.noreply.github.com>
2018-02-25 13:56:39 +0100

    :sparkles: :lipstick: Add external css for home page

--- local_code/resources/views/authenticated/accueil.blade.php
+++ local_code/resources/views/authenticated/accueil.blade.php
@@ -2,6 +2,7 @@
 
 @push('styles')
     <link href="{{ asset('css/apercuCours.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/accueil.css') }}" rel="stylesheet">
 @endpush
 
 @section('title', 'Accueil')
@@ -13,19 +14,19 @@
     @php($view = 'light')
     @php($angles = array(5, 1, -2, -6, 4, -1, 2, -5, -4, 6))
 
-    <h3 style="color: #FFF;">Bienvenue {{ Auth::user()->username }} !</h3>
+    <h3>Bienvenue {{ Auth::user()->username }} !</h3>
 
     <div class="container">
         <div class="row">
-            <div class="col col-12 col-sm-10 offset-sm-1" style="color: #FFF;">
-                Pour l'instant, vous êtes inscrit à <span class="green" style="color: #2AB77E">{{ App\Enrollment::numberOfEnrollments() }}</span> cours et vous en avez placé <span class="green" style="color: #2AB77E">{{ App\Favorite::numberOfFavorites() }}</span> dans vos favoris.
+            <div class="col col-12 col-sm-10 offset-sm-1 white">
+                Pour l'instant, vous êtes inscrit à <span class="green">{{ App\Enrollment::numberOfEnrollments() }}</span> cours et vous en avez placé <span class="green">{{ App\Favorite::numberOfFavorites() }}</span> dans vos favoris.
             </div>
         </div>
         <div class="row previewParent">
-            <div class="col-11"><h2 style="padding-bottom: 20px;"><i class="fas fa-star" style="color: #FCDB69"></i> Favoris</h2></div>
+            <div class="col-11"><h2 id="star"><i class="fas fa-star"></i> Favoris</h2></div>
 
             @if (App\Favorite::numberOfFavorites() == 0)
-                <h4 style="color: #FFF;">Vous n'avez pas encore de favoris. <i class="far fa-frown"></i></h4>
+                <h4 class="void">Vous n'avez pas encore de favoris. <i class="far fa-frown"></i></h4>
             @else
                 @foreach ($favs as $course)
                     @include('authenticated.layouts.apercuCours')
@@ -35,10 +36,10 @@
 
         </div>
             <div class="row">
-                <div class="col-11"><h2 style="padding-bottom: 20px;"><i class="fas fa-bookmark" style="color: #DC4535"></i> Inscrits</h2></div>
+                <div class="col-11"><h2 id="bookm"><i class="fas fa-bookmark"></i> Inscrits</h2></div>
 
                 @if (App\Enrollment::numberOfEnrollments() == 0)
-                    <h4 style="color: #FFF;">Vous n'êtes pas encore inscrit à un cours. <i class="far fa-frown"></i></h4>
+                    <h4 class="void">Vous n'êtes pas encore inscrit à un cours. <i class="far fa-frown"></i></h4>
                 @else
                     @foreach ($enrs as $course)
                         @include('authenticated.layouts.apercuCours')
