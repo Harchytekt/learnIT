f100e991fee35d21500506afd60267bb0b29f36c
Alexandre Ducobu <Harchytekt@users.noreply.github.com>
2018-03-18 18:13:18 +0100

    :sparkles: :lipstick: :bento: Add Syntax highlighting
    With https://highlightjs.org/

--- server_code/resources/views/authenticated/chapitre.blade.php
+++ server_code/resources/views/authenticated/chapitre.blade.php
@@ -4,6 +4,7 @@
 <link href="{{ asset('css/course.css') }}" rel="stylesheet">
 <link href="{{ asset('css/courseContent.css') }}" rel="stylesheet">
 <link href="{{ asset('css/firaCode.css') }}" rel="stylesheet"">
+<link href="{{ asset('css/highlightStyles/atom-one-dark.css') }}" rel="stylesheet"">
 @endpush
 
 <title>{{ $chapter->name }}</title>
@@ -14,4 +15,6 @@
 
 @push('js')
     <script src="{{ asset('js/chapitre.js') }}"></script>
+    <script src="{{ asset('js/highlight.pack.js') }}"></script>
+    <script>hljs.initHighlightingOnLoad();</script>
 @endpush
