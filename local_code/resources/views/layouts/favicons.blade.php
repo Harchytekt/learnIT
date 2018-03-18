2ecea46d4f1f09b73722e8ad61f024f37ad016c0
Alexandre Ducobu <Harchytekt@users.noreply.github.com>
2018-03-18 19:45:37 +0100

    :bento: Update favicons
    Tool: https://realfavicongenerator.net

--- local_code/resources/views/layouts/favicons.blade.php
+++ local_code/resources/views/layouts/favicons.blade.php
@@ -1,29 +1,9 @@
-<!-- For old IEs -->
-<link rel="shortcut icon" href="{{ asset('img/favicons/favicon.ico') }}" />
-<!-- For new browsers - multisize ico  -->
-<link rel="icon" type="image/x-icon" sizes="16x16 32x32" href="{{ asset('img/favicons/favicon.ico') }}">
-<!-- For Safari's Pinned Tab -->
-<link rel="mask-icon" href="{{ asset('img/favicons/safari-pinned-tab.svg') }}" color="#2cc185">
-<!-- For iPad with high-resolution Retina display running iOS ≥ 7: -->
-<link rel="apple-touch-icon-precomposed" sizes="152x152" href="{{ asset('img/favicons/favicon-152.png') }}">
-<!-- For iPad with high-resolution Retina display running iOS ≤ 6: -->
-<link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{ asset('img/favicons/favicon-144.png') }}">
-<!-- For iPhone with high-resolution Retina display running iOS ≥ 7: -->
-<link rel="apple-touch-icon-precomposed" sizes="120x120" href="{{ asset('img/favicons/favicon-120.png') }}">
-<!-- For iPhone with high-resolution Retina display running iOS ≤ 6: -->
-<link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{ asset('img/favicons/favicon-114.png') }}">
-<!-- For iPhone 6+ -->
-<link rel="apple-touch-icon-precomposed" sizes="180x180" href="{{ asset('img/favicons/favicon-180.png') }}">
-<!-- For first- and second-generation iPad: -->
-<link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{ asset('img/favicons/favicon-72.png') }}">
-<!-- For non-Retina iPhone, iPod Touch, and Android 2.1+ devices: -->
-<link rel="apple-touch-icon-precomposed" href="{{ asset('img/favicons/favicon.ico') }}favicon-57.png">
-<!-- For Old Chrome -->
-<link rel="icon" href="{{ asset('img/favicons/favicon-32.png') }}" sizes="32x32">
-<!-- For IE10 Metro -->
-<meta name="msapplication-TileColor" content="#FFFFFF">
-<meta name="msapplication-TileImage" content="{{ asset('img/favicons/favicon-144.png') }}">
-<meta name="theme-color" content="#ffffff">
-<!-- Chrome for Android -->
-<link rel="manifest" href="resources/favicons/manifest.json">
-<link rel="icon" sizes="192x192" href="{{ asset('img/favicons/favicon-192.png') }}">
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('img/favicons/apple-touch-icon.png?v=2') }}">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/favicons/favicon-32x32.png?v=2') }}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/favicons/favicon-16x16.png?v=2') }}">
+<link rel="manifest" href="{{ asset('img/favicons/site.webmanifest?v=2') }}">
+<link rel="mask-icon" href="{{ asset('img/favicons/safari-pinned-tab.svg?v=2" color="#2AB77E') }}">
+<link rel="shortcut icon" href="{{ asset('img/favicons/favicon.ico?v=2') }}">
+<meta name="msapplication-TileColor" content="#2AB77E">
+<meta name="msapplication-config" content="{{ asset('img/favicons/browserconfig.xml?v=2') }}">
+<meta name="theme-color" content="#FFFFFF">
