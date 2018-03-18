208a443a02527173df2120d45981aa8a1a84a56d
Alexandre Ducobu <Harchytekt@users.noreply.github.com>
2018-03-18 19:50:42 +0100

    :bug: Fix link to the safari pinned tab favicon

--- local_code/resources/views/layouts/favicons.blade.php
+++ local_code/resources/views/layouts/favicons.blade.php
@@ -2,7 +2,7 @@
 <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/favicons/favicon-32x32.png?v=2') }}">
 <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/favicons/favicon-16x16.png?v=2') }}">
 <link rel="manifest" href="{{ asset('img/favicons/site.webmanifest?v=2') }}">
-<link rel="mask-icon" href="{{ asset('img/favicons/safari-pinned-tab.svg?v=2" color="#2AB77E') }}">
+<link rel="mask-icon" href="{{ asset('img/favicons/safari-pinned-tab.svg?v=2') }}" color="#2AB77E'">
 <link rel="shortcut icon" href="{{ asset('img/favicons/favicon.ico?v=2') }}">
 <meta name="msapplication-TileColor" content="#2AB77E">
 <meta name="msapplication-config" content="{{ asset('img/favicons/browserconfig.xml?v=2') }}">
