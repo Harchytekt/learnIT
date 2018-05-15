99c628d3ed36da3490666ce312e370e8ba334baf
Alexandre Ducobu <Harchytekt@users.noreply.github.com>
2018-05-15 15:58:51 +0200

    :bento: Add course to fav/enroll everywhere

--- local_code/resources/views/authenticated/layouts/courses/apercuCours.blade.php
+++ local_code/resources/views/authenticated/layouts/courses/apercuCours.blade.php
@@ -10,24 +10,22 @@
         @if ($view != 'light')
             <hr>
             <p>{{ $course->description }}</p>
-            @if ($view == 'all')
-                <div class="previewIcons" hiddenValue="{{ $course->id }}">
-					<a href="/coursinscrits/{{ $course->id }}">
-	                    @if ($course->isEnrollment())
-	                        <i class="fas fa-bookmark active"></i>
-	                    @else
-	                        <i class="fas fa-bookmark"></i>
-	                    @endif
-					</a>
-					<a href="/favoris/{{ $course->id }}">
-	                    @if ($course->isFavorite())
-							<i class="fas fa-star active"></i>
-	                    @else
-	                        <i class="fas fa-star"></i>
-	                    @endif
-					</a>
-                </div>
-            @endif
+            <div class="previewIcons" hiddenValue="{{ $course->id }}">
+				<a href="/coursinscrits/{{ $course->id }}">
+                    @if ($course->isEnrollment())
+                        <i class="fas fa-bookmark active"></i>
+                    @else
+                        <i class="fas fa-bookmark"></i>
+                    @endif
+				</a>
+				<a href="/favoris/{{ $course->id }}">
+                    @if ($course->isFavorite())
+						<i class="fas fa-star active"></i>
+                    @else
+                        <i class="fas fa-star"></i>
+                    @endif
+				</a>
+            </div>
         @endif
         @if ($course->published)
             <a href="/cours/{{ $course->id }}" class="btn btn-link courseLink">Voir le cours</a> <!-- $course->id -->
