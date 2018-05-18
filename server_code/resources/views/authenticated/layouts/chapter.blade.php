7c4fd3ccf51d27c0c0a16960c5521d32163e1af5
Alexandre Ducobu <Harchytekt@users.noreply.github.com>
2018-05-18 17:56:03 +0200

    :bug: Fix enclosed div

--- server_code/resources/views/authenticated/layouts/chapter.blade.php
+++ server_code/resources/views/authenticated/layouts/chapter.blade.php
@@ -47,11 +47,15 @@
 			<hr>
 		@endif
 
-        <h6><a href="/cours/{{ $course->id }}" class="tip" data-toggle="tooltip" data-placement="top" title="Retour à la description du cours">{{ $course->name }}</a></h6>
+        <h6>
+			<a href="/cours/{{ $course->id }}" class="tip" data-toggle="tooltip" data-placement="top" title="Retour à la description du cours">{{ $course->name }}</a>
+		</h6>
         <h1 style="text-align: center; padding-bottom: 20px;">{{ $chapter->order_id }}. {{ $chapter->name }}</h1>
 
-        <div class="pagination-container">{!! $chapter->getBody() !!}</div> <!-- entities escape  -->
+        <div class="pagination-container">{!! $chapter->getBody() !!}</div>
+		<!-- entities escape  -->
+	</div>
 
-    <img class="pushpin bottom left" src="{{ asset('img/pushpin2.svg') }}" alt="" height="65" width="65">
+	<img class="pushpin bottom left" src="{{ asset('img/pushpin2.svg') }}" alt="" height="65" width="65">
     <img class="pushpin right bottom" src="{{ asset('img/pushpin2.svg') }}" alt="" height="65" width="65">
 </div>
