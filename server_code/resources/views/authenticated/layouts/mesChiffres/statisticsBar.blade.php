2ea8847561aa9f47277b2b460e9a7dfccd90e178
Alexandre Ducobu <alexandre.ducobu@yahoo.be>
2018-05-27 19:57:30 +0200

    :iphone: Update responsive design

--- server_code/resources/views/authenticated/layouts/mesChiffres/statisticsBar.blade.php
+++ server_code/resources/views/authenticated/layouts/mesChiffres/statisticsBar.blade.php
@@ -1,23 +1,23 @@
 <div class="row" id="statisticsBar">
-	<div class="col-10 offset-1 col-md-5 col-lg-4 col-xl-2">
+	<div class="col-5 offset-sm-1 col-md-2 offset-md-0">
 		<div class="statisticsBubble">
 			<h2>{{ App\Enrollment::numberOfEnrollments() }}</h2>
 			<p>cours suivis</p>
 		</div>
 	</div>
-	<div class="col-10 offset-1 col-md-5 col-lg-4 col-xl-2">
+	<div class="col-5 offset-1 col-md-2">
 		<div class="statisticsBubble">
 			<h2>{{ App\Enrollment::numberOfCompletedEnrollments() }}</h2>
 			<p>cours réussis</p>
 		</div>
 	</div>
-	<div class="col-10 offset-1 col-md-5 col-lg-4 col-xl-2">
+	<div class="col-5 offset-sm-1 col-md-2 offset-lg-2">
 		<div class="statisticsBubble">
 			<h2>{{ App\Chapter::numberOfPassedChapters() }}</h2>
 			<p>chapitres réussis</p>
 		</div>
 	</div>
-	<div class="col-10 offset-1 col-md-5 col-lg-4 col-xl-2">
+	<div class="col-5 offset-1 col-md-2">
 		<div class="statisticsBubble">
 			<h2 id="percentage">{{ App\Enrollment::getAverage() }}</h2> <h4>%</h4>
 			<p>de moyenne</p>
