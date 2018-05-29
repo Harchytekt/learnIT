689d0a034a5c76dd50995d2c73a745ea9f6f783e
Alexandre Ducobu <alexandre.ducobu@yahoo.be>
2018-05-29 15:28:21 +0200

    :bug: Fix html entities in quiz

--- local_code/js/editQuiz.js
+++ local_code/js/editQuiz.js
@@ -2,6 +2,31 @@
 
 var previous = {}, nbOfQuestions = 0, currentNbOfQuestions, maxNbOfQuestions = 10;
 var file;
+
+
+/**
+* This function/object is used to serialize
+* a formulaire into a format compatible with
+* the one used for the REST call through ajax.
+*
+* The code comes from:
+* https://stackoverflow.com/questions/12966433/convert-form-data-to-json-object
+*/
+$.fn.serializeObject = function() {
+	let quiz = { questions : [] }, currentQuestion = {};
+	let questionNumber = 0;
+	let a = this.serializeArray();
+	for (let i = 0; i < a.length; i++) {
+		if (a[i].name == `title`) {
+			currentQuestion = {};
+			currentQuestion[a[i].name] = a[i].value;
+			currentQuestion.choices = getChoices(a, i + 1);
+			quiz.questions[questionNumber++] = currentQuestion;
+		}
+	}
+	return quiz;
+};
+
 $(document).ready(function() {
 	if (data == 0) {
 		initQuizEditor();
@@ -33,41 +58,58 @@ $(document).on('click', '#removeQuestion', function() {
 	removeLastQuestion();
 });
 
+// Import the given quiz if the file extension is good.
+$('#importQuiz').on('click', '#importQ', function(event) {
+	event.stopPropagation();
+	event.preventDefault();
+	readFile(file, function(e) {
+		saveChanges(JSON.parse(e.target.result));
+	});
+});
+
+// Verify if the given file is '.json'.
+$('input[type="file"]').on('change', function() {
+	var ext = $(this).val().split('.')[1];
+
+	if (ext == 'json') {
+		$('#importQ').attr('disabled', false);
+		$('#error').css('display', 'none');
+		$('#error').html('');
+		file = $(this)[0].files[0];
+	} else {
+		$('#importQ').attr('disabled', true);
+		$('#error').html(`⚠️ Seuls les fichiers dont l'extension est <wrong>.json</wrong> sont acceptés !`);
+		$('#error').css('display', 'block');
+	}
+});
+
 /**
- * This function is used to initialise the quiz editor by
- * setting an empty form.
+ * This function is used to add the three choices
+ * of a question in the quiz form.
  *
- * @param quizQuestionsNumber
- *		The number of questions in the form.
- *		By default it's set to 2.
+ * @return the html code of the choices.
  */
-function initQuizEditor(quizQuestionsNumber = 2) {
-	let res = `<form class="form-horizontal">`;
-	for (let i = 0; i < quizQuestionsNumber; i++) {
-		res += addNewQuestion(true);
+function addChoices() {
+	let res = ``;
+	for (let i = 1; i <= 3; i++) {
+		res += `<div class="form-group">
+			<div class="form-group">
+				<label for="choice${ i }Q${ currentNbOfQuestions }" class="control-label">Choix N°${ i } : </label>
+				<input id="choice${ i }Q${ currentNbOfQuestions }" class="form-control" type="text" name="choice" required>
+			</div>
+			<div class="form-group">
+				<label for="explanation${ i }Q${ currentNbOfQuestions }" class="control-label">Explication : </label>
+				<input id="explanation${ i }Q${ currentNbOfQuestions }" class="form-control" type="text" name="explanation" required>
+			</div>
+			<div class="form-group">
+				<div class="custom-control custom-radio">
+					<input type="radio" class="custom-control-input" name="answersQ${ currentNbOfQuestions }" id="answer${ i }Q${ currentNbOfQuestions }" ${ (i == 1) ? "checked" : "" }>
+					<label class="custom-control-label" for="answer${ i }Q${ currentNbOfQuestions }" class="control-label">${ (i == 1) ? "vrai" : "faux" }</label>
+				</div>
+			</div>
+		</div>`;
 	}
-	res += `</form>`;
-	$('.quiz').html(res);
-}
-
-/**
- * This function is used to set the data from the quiz
- * to modify inside the form.
- */
-function getQuizData() {
-	let currentQuestion, currentChoices;
-	for (let i = 0; i < data.questions.length; i++) {
-		currentQuestion = data.questions[i];
-		$(`#titleQ${ i + 1 }`).val(currentQuestion.title);
-
-		for (let j = 0; j < currentQuestion.choices.length; j++) {
-			currentChoices = currentQuestion.choices[j];
-			$(`#choice${ j + 1 }Q${ i + 1 }`).val(currentChoices.choice);
-			$(`#explanation${ j + 1 }Q${ i + 1 }`).val(currentChoices.explanation);
-			$(`#answer${ j + 1 }Q${ i + 1 }`).attr(`checked`, currentChoices.isTheAnswer);
-			$(`label[for="answer${ j + 1 }Q${ i + 1 }"]`).text((currentChoices.isTheAnswer) ? 'vrai' : 'faux');
-		}
-	}
+	return res;
 }
 
 /**
@@ -101,71 +143,6 @@ function addNewQuestion(isInit = false) {
 }
 
 /**
- * This function is used to remove the last question of the form.
- * The minimal number of question has to be 2, so the function cannot
- * be used unless there are three or more questions.
- */
-function removeLastQuestion() {
-	if (nbOfQuestions > 2) {
-		$(`#Q${ nbOfQuestions-- }`).remove();
-	}
-
-	updateButtonState();
-}
-
-/**
- * This function is used to add the three choices
- * of a question in the quiz form.
- *
- * @return the html code of the choices.
- */
-function addChoices() {
-	let res = ``;
-	for (let i = 1; i <= 3; i++) {
-		res += `<div class="form-group">
-			<div class="form-group">
-				<label for="choice${ i }Q${ currentNbOfQuestions }" class="control-label">Choix N°${ i } : </label>
-				<input id="choice${ i }Q${ currentNbOfQuestions }" class="form-control" type="text" name="choice" required>
-			</div>
-			<div class="form-group">
-				<label for="explanation${ i }Q${ currentNbOfQuestions }" class="control-label">Explication : </label>
-				<input id="explanation${ i }Q${ currentNbOfQuestions }" class="form-control" type="text" name="explanation" required>
-			</div>
-			<div class="form-group">
-				<div class="custom-control custom-radio">
-					<input type="radio" class="custom-control-input" name="answersQ${ currentNbOfQuestions }" id="answer${ i }Q${ currentNbOfQuestions }" ${ (i == 1) ? "checked" : "" }>
-					<label class="custom-control-label" for="answer${ i }Q${ currentNbOfQuestions }" class="control-label">${ (i == 1) ? "vrai" : "faux" }</label>
-				</div>
-			</div>
-		</div>`;
-	}
-	return res;
-}
-
-/**
-* This function/object is used to serialize
-* a formulaire into a format compatible with
-* the one used for the REST call through ajax.
-*
-* The code comes from:
-* https://stackoverflow.com/questions/12966433/convert-form-data-to-json-object
-*/
-$.fn.serializeObject = function() {
-	let quiz = { questions : [] }, currentQuestion = {};
-	let questionNumber = 0;
-	let a = this.serializeArray();
-	for (let i = 0; i < a.length; i++) {
-		if (a[i].name == `title`) {
-			currentQuestion = {};
-			currentQuestion[a[i].name] = a[i].value;
-			currentQuestion.choices = getChoices(a, i + 1);
-			quiz.questions[questionNumber++] = currentQuestion;
-		}
-	}
-	return quiz;
-};
-
-/**
  * This function is used to get the current question's choices.
  *
  * @param data
@@ -181,7 +158,7 @@ function getChoices(data, nb) {
 		if (data[i].name == `choice`) {
 			currentChoice = {};
 			goodAnswer = false;
-			currentChoice[data[i].name] = data[i].value;
+			currentChoice[data[i].name] = htmlEntitiesToText(data[i].value);
 			currentChoice[data[i+1].name] = data[i+1].value;
 			if (i+2 != nb + nbFields) {
 				goodAnswer = (data[i+2].name.slice(0, 7) == "answers");
@@ -192,6 +169,94 @@ function getChoices(data, nb) {
 		i++;
 	}
 	return choices;
+}
+
+/**
+ * This function is used to set the data from the quiz
+ * to modify inside the form.
+ */
+function getQuizData() {
+	let currentQuestion, currentChoices;
+	for (let i = 0; i < data.questions.length; i++) {
+		currentQuestion = data.questions[i];
+		$(`#titleQ${ i + 1 }`).val(currentQuestion.title);
+
+		for (let j = 0; j < currentQuestion.choices.length; j++) {
+			currentChoices = currentQuestion.choices[j];
+			$(`#choice${ j + 1 }Q${ i + 1 }`).val(textToHtmlEntities(currentChoices.choice));
+			$(`#explanation${ j + 1 }Q${ i + 1 }`).val(currentChoices.explanation);
+			$(`#answer${ j + 1 }Q${ i + 1 }`).attr(`checked`, currentChoices.isTheAnswer);
+			$(`label[for="answer${ j + 1 }Q${ i + 1 }"]`).text((currentChoices.isTheAnswer) ? 'vrai' : 'faux');
+		}
+	}
+}
+
+/**
+ * This function is used to transform HTML entities into text.
+ *
+ * @param str
+ *		The string containing the HTML entities.
+ *
+ * Source: https://css-tricks.com/snippets/javascript/htmlentities-for-javascript/
+ */
+function htmlEntitiesToText(str) {
+    return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+}
+
+/**
+ * This function is used to initialise the quiz editor by
+ * setting an empty form.
+ *
+ * @param quizQuestionsNumber
+ *		The number of questions in the form.
+ *		By default it's set to 2.
+ */
+function initQuizEditor(quizQuestionsNumber = 2) {
+	let res = `<form class="form-horizontal">`;
+	for (let i = 0; i < quizQuestionsNumber; i++) {
+		res += addNewQuestion(true);
+	}
+	res += `</form>`;
+	$('.quiz').html(res);
+}
+
+/**
+ * This function is used to get the given file from the form.
+ *
+ * @param file
+ *		The object containing the file content.
+ * @param callback
+ *		The callback used to get the content of the file.
+ */
+function readFile(file, callback) {
+	if (typeof (FileReader) != "undefined") {
+		var reader = new FileReader();
+		reader.onload = callback;
+		reader.readAsText(file);
+	}
+}
+
+/**
+ * This function is used to remove the last question of the form.
+ * The minimal number of question has to be 2, so the function cannot
+ * be used unless there are three or more questions.
+ */
+function removeLastQuestion() {
+	if (nbOfQuestions > 2) {
+		$(`#Q${ nbOfQuestions-- }`).remove();
+	}
+
+	updateButtonState();
+}
+
+/**
+ * This function is used to transform text into HTML entities.
+ *
+ * @param str
+ *		The string containing the text.
+ */
+function textToHtmlEntities(str) {
+    return String(str).replace('&amp;', '&').replace('&lt;', '<').replace('&gt;', '>').replace('&quot;', '"');
 }
 
 /**
@@ -231,45 +296,4 @@ function saveChanges(data = null) {
 function updateButtonState() {
 	$(`#addQuestion`).prop('disabled', (nbOfQuestions >= maxNbOfQuestions));
 	$(`#removeQuestion`).prop('disabled', (nbOfQuestions > 2) ? false : true);
-}
-
-// Verify if the given file is '.json'.
-$('input[type="file"]').on('change', function() {
-	var ext = $(this).val().split('.')[1];
-
-	if (ext == 'json') {
-		$('#importQ').attr('disabled', false);
-		$('#error').css('display', 'none');
-		$('#error').html('');
-		file = $(this)[0].files[0];
-	} else {
-		$('#importQ').attr('disabled', true);
-		$('#error').html(`⚠️ Seuls les fichiers dont l'extension est <wrong>.json</wrong> sont acceptés !`);
-		$('#error').css('display', 'block');
-	}
-});
-
-// Import the given quiz if the file extension is good.
-$('#importQuiz').on('click', '#importQ', function(event) {
-	event.stopPropagation();
-	event.preventDefault();
-	readFile(file, function(e) {
-		saveChanges(JSON.parse(e.target.result));
-	});
-});
-
-/**
- * This function is used to get the given file from the form.
- *
- * @param file
- *		The object containing the file content.
- * @param callback
- *		The callback used to get the content of the file.
- */
-function readFile(file, callback) {
-	if (typeof (FileReader) != "undefined") {
-		var reader = new FileReader();
-		reader.onload = callback;
-		reader.readAsText(file);
-	}
 }
